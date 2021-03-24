7eb2df6b3895bcee0b030e51e2658604dadd914b
jesusm-synergy <81315898+jesusm-synergy@users.noreply.github.com>
2021-03-24 17:12:43 -0400

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -1,93 +1,336 @@
 import json
 import boto3
-from datetime import datetime
+import re
+from datetime import datetime, date, time, timezone
 
-s3 = boto3.resource('s3')
-ec2 = boto3.resource('ec2')
-
+datenow= datetime.now()
+now= datenow.strftime("%Y-%m-%d %H:%M:%S")
 
 def lambda_handler(event, context):
-       
-    event= event.get("responsePayload")
+    
+    # it overwrites event variable with the event coming from previous lambda
+    event= json.loads(event['Records'][0]['Sns']['Message'])
+    
+    # We capture and Filter the evenName -  if not a creation event it does not process the tag Creator
+    # if itis a  creation event it does process the tag Creator, it passes the event, userIdentity and awsRegion coming from json
     
     eventName=event["detail"].get("eventName")
-    principaID= event["detail"]["userIdentity"].get("arn")
+    
     region=event["detail"].get("awsRegion")
-
-    client = boto3.client('ec2', region_name = region)
+    userName = event["detail"]["userIdentity"].get("sessionContext")
+    principaID= event["detail"]["userIdentity"].get("arn")
     
-    if(eventName == "CreateSubnet"):
-        subnetID= event["detail"]["responseElements"]["subnet"].get("subnetId")
-        addTagClient(client,principaID,subnetID)
+    if(userName != None):
+        userName= userName["sessionIssuer"].get("userName")
+    else:
+        userName= event["detail"]["userIdentity"].get("userName")
+    
+
+    #CreateStack
+    if(eventName == "CreateStack"):
+        cloudformation = boto3.client('cloudformation' )
+        stack= event["detail"]["requestParameters"].get("stackName")
+        url = event["detail"]["requestParameters"].get("templateURL")
+        addTagStack(cloudformation,userName,principaID, stack , url)
+    
+
+    #ParameterStore
+    elif(eventName == "PutParameter"):
+        resourcegroupstaggingapi= boto3.client('resourcegroupstaggingapi' )
+        projectArn= event["detail"]["resources"][0]["ARN"]
+        addtagResource(resourcegroupstaggingapi,userName,principaID, projectArn)
+    
+
+    #Pipeline
+    elif(eventName == "CreatePipeline"):
+        resourcegroupstaggingapi= boto3.client('resourcegroupstaggingapi' )
+        codepipeline= boto3.client('codepipeline' )
+        codepipelineArn= event["detail"]["responseElements"]["pipeline"].get("name")
+        responsepipeline= codepipeline.get_pipeline(name = codepipelineArn)
+        codepipelineArn = responsepipeline["metadata"].get("pipelineArn")
+        addtagResource(resourcegroupstaggingapi,userName,principaID, codepipelineArn)
+    
+
+    #CodeBuild
+    elif(eventName == "CreateProject"):
+        resourcegroupstaggingapi= boto3.client('resourcegroupstaggingapi' )
+        projectArn= event["detail"]["requestParameters"].get("name")
+        addtagResource(resourcegroupstaggingapi,userName,principaID, projectArn)
+     
+    #CLOUDTRAIL
+    elif(eventName == "CreateTrail"):
+        clientTrail= boto3.client('cloudtrail' )
+        trailArn= event["detail"]["responseElements"].get("trailARN")
+        addTagTrail(clientTrail,userName,principaID, trailArn)
+                
+
+    #TOPIC SQS
+    elif(eventName == "CreateQueue"):
+        resourcegroupstaggingapi= boto3.client('resourcegroupstaggingapi' )
+        queueArn= event["detail"]["requestParameters"].get("queueName")
+        queueArn = "arn:aws:sqs:"+region+":" +str(event.get("account"))+":"+queueArn
+        addtagResource(resourcegroupstaggingapi,userName,principaID, queueArn)    
+    #TOPIC SNS
+    elif(eventName == "CreateTopic"):
+        resourcegroupstaggingapi= boto3.client('resourcegroupstaggingapi' )
+        topicArn= event["detail"]["responseElements"].get("topicArn")
+        addtagResource(resourcegroupstaggingapi,userName,principaID, topicArn)
+    #LAMBDA    
+    elif(eventName == "CreateFunction20150331"):
+        functionArn=event["detail"]["responseElements"]
+        if(type(functionArn) == dict):
+            functionArn=functionArn.get("functionArn")
+            print(functionArn)
+            resourcegroupstaggingapi= boto3.client('resourcegroupstaggingapi' )
+            addtagResource(resourcegroupstaggingapi,userName,principaID, functionArn)
+        else:
+            print("NO ")
+            
+    #CLOUDWATCH
+    elif(eventName == "PutRule"):
+        resourcegroupstaggingapi= boto3.client('resourcegroupstaggingapi' )
+        ruleArn= event["detail"]["responseElements"].get("ruleArn")
+        addtagResource(resourcegroupstaggingapi,userName,principaID, ruleArn)
         
-    elif(eventName == "CreateSecurityGroup"):
-        groupId= event["detail"]["responseElements"].get("groupId")
-        addTagClient(client,principaID,groupId)
+    elif(eventName == "CreateLogGroup"):
+        resourcegroupstaggingapi= boto3.client('logs' )
+        logArn= event["detail"]["requestParameters"].get("logGroupName")
+        addtagLogGruop(resourcegroupstaggingapi,userName,principaID, logArn)
+    #IAM
+    elif(eventName == "CreateUser"):
+        iam= boto3.client('iam' )
+        userNamec= event["detail"]["responseElements"]["user"].get("userName")
+        addTagUser(iam,userName,principaID, userNamec)
+
+    elif(eventName == "CreateRole"):
+        iam= boto3.client('iam' )
+        roleName= event["detail"]["responseElements"]["role"].get("roleName")
+        addTagRole(iam,userName,principaID, roleName)
+    
+    elif(eventName =="CreatePolicy"):
+        iam= boto3.client('iam' )
+        PolicyArn= event["detail"]["responseElements"]["policy"].get("arn")
+        addTagPolicy(iam,userName,principaID, PolicyArn)
+    #S3
+    elif(eventName == "CreateBucket"):
+        resourcegroupstaggingapi= boto3.client('resourcegroupstaggingapi' )
+        bucket_name= event["detail"]["requestParameters"].get("bucketName")
+        bucket_name ="arn:aws:s3:::"+bucket_name
+        addtagResource(resourcegroupstaggingapi,userName,principaID, bucket_name)
         
-    elif(eventName == "CreateVpc"):
-        vpcId=event["detail"]["responseElements"]["vpc"].get("vpcId")
-        addTagClient(client,principaID,vpcId)
+    elif(eventName == "PutObject"):    
+        resourcegroupstaggingapi= boto3.client('resourcegroupstaggingapi' )
+        logArn= event["detail"]["requestParameters"].get("logGroupName")
+        logArn = "arn:aws:sqs:"+region+":" +str(event.get("account"))+":"+logArn
+        addtagResource(resourcegroupstaggingapi,userName,principaID, logArn)
         
-    elif(eventName == "CreateRouteTable"):
-        routeTableId=event["detail"]["responseElements"]["routeTable"].get("routeTableId")
-        addTagClient(client,principaID,routeTableId)
-
-    elif(eventName == "CreateInternetGateway"):
-        igwId=event["detail"]["responseElements"]["internetGateway"].get("internetGatewayId")
-        addTagClient(client,principaID,igwId)
-
-    elif(eventName == "CreateNetworkAcl"):
-        naclId=event["detail"]["responseElements"]["networkAcl"].get("networkAclId")
-        addTagClient(client,principaID,naclId)
-
-    elif(eventName == "CreateNetworkInterface"):
-        naclId=event["detail"]["responseElements"]["networkInterface"].get("networkInterfaceId")
-        addTagClient(client,principaID,naclId)
-
-    elif(eventName == "CreateSnapshot"):
-        snapshoID=event["detail"]["responseElements"].get("snapshotId")
-        addTagSnapshot(principaID, snapshoID)
-
-    elif(eventName == "CreateVolume"):
-        volumeId=event["detail"]["responseElements"].get("volumeId")
-        addTagClient(client,principaID,volumeId)
+    #EC2
+    elif("Create" in eventName or (eventName in ["RunInstances","AllocateAddress"])):
         
-    elif(eventName == "RunInstances"):
-        instanceId=event["detail"]["responseElements"]["instancesSet"]["items"][0].get("instanceId")
-        addTagClient(client,principaID,instanceId)
-
-    elif(eventName == "CreateImage"):
-        instanceId=event["detail"]["responseElements"].get("imageId")
-        addTagClient(client,principaID,instanceId)
+        client = boto3.client('ec2' )
+        response =  event["detail"]["responseElements"]
+        request = event["detail"]["requestParameters"]
     
-    elif(eventName == "CreateTransitGateway"):
-        idTgw=instanceId=event["detail"]["responseElements"]["CreateTransitGatewayResponse"]["transitGateway"].get("transitGatewayId")
-        addTagClient(client,principaID, idTgw)
+        request_ids=[]
+        response_ids = []
+        
+        for request_id in item_generator(request):
+            request_ids.append(request_id)
+            
+        for response_id in item_generator(response):
+            if (response_id not in request_ids):
+                if(eventName == "RunInstances" and "vpc-" not in response_id):
+                    response_ids.append(response_id)
+                elif(eventName != "RunInstances"):
+                    response_ids.append(response_id)
+        
+        for ids in response_ids:
+            addTagClient(client,userName,principaID, ids)
+    else:
+        return "Not is event creation"
 
-    elif(eventName == "CreateTransitGatewayVpcAttachment"):
-        idTgwA=instanceId=event["detail"]["responseElements"]["CreateTransitGatewayVpcAttachmentResponse"]["transitGatewayVpcAttachment"].get("transitGatewayAttachmentId")
-        addTagClient(client,principaID, idTgwA)
-    
-    elif(eventName == "AllocateAddress"):
-        allocateid=instanceId=event["detail"]["responseElements"].get("allocationId")
-        addTagClient(client,principaID, allocateid)
-    
-    elif(eventName == "CreateNatGateway"):
-        natgwId=event["detail"]["responseElements"]["CreateNatGatewayResponse"]["natGateway"].get("natGatewayId")
-        addTagClient(client,principaID, natgwId)
-    
+# addTagClient - function to create tags in all resources newly created
+# it adds the value principalID to creatorID tag to each resource created
+def addTagClient(client,userName,principaID, ids):
+    try:
+        response = client.create_tags(
+            Resources=[
+                ids,
+            ],
+            Tags=[
+                {
+                    'Key': 'creatorId',
+                    'Value': principaID,
+                },
+                {
+                    'Key': 'UserName',
+                    'Value': userName,
+                },
+                {
+                    'Key': 'create_at',
+                    'Value': now
+                },
+            ],
+        )
+    except Exception as e:
+        print (e)
+        
+        
+        
+        
+def addTagStack(client,userName,principaID, ids, url):
+    try:
+        response = client.update_stack(
+            StackName= ids,
+            TemplateURL = url,
+            Tags=[
+                {
+                    'Key': 'creatorId',
+                    'Value': principaID,
+                },
+                {
+                    'Key': 'UserName',
+                    'Value': userName,
+                },
+                {
+                    'Key': 'create_at',
+                    'Value': now
+                },
+            ],
+        )
+    except Exception as e:
+        print (e)        
+        
+def addTagTrail(client,userName,principaID, ids):
+    try:
+        response = client.add_tags(
+            ResourceId= ids,
+            TagsList=[
+                {
+                    'Key': 'creatorId',
+                    'Value': principaID,
+                },
+                {
+                    'Key': 'UserName',
+                    'Value': userName,
+                },
+                {
+                    'Key': 'create_at',
+                    'Value': now
+                },
+            ],
+        )
+    except Exception as e:
+        print (e)
+                
 
+def addTagRole(client,userName,principaID, rolename):
+    try:
+        response = client.tag_role(
+            RoleName=rolename,
+            Tags=[
+                {
+                    'Key': 'creatorId',
+                    'Value': principaID,
+                },
+                {
+                    'Key': 'UserName',
+                    'Value': userName,
+                },
+                {
+                    'Key': 'create_at',
+                    'Value': now
+                },
+            ],
+        )
+    except Exception as e:
+        print (e)
+        
+def addTagUser(client,userName,principaID, username):
+    try:
+        response = client.tag_user(
+            UserName=username,
+            Tags=[
+                {
+                    'Key': 'creatorId',
+                    'Value': principaID,
+                },
+                {
+                    'Key': 'UserName',
+                    'Value': userName,
+                },
+                {
+                    'Key': 'create_at',
+                    'Value': now
+                },
+            ],
+        )
+    except Exception as e:
+        print (e)
+        
 
-#ClientTags
-def addTagClient(client,principaID, ids):
-    response = client.create_tags(
-        Resources=[
-            ids,
-        ],
-        Tags=[
-            {
-                'Key': 'creatorID',
-                'Value': principaID,
-            },
-        ],
-    )
+def addTagPolicy(client,userName,principaID, policy):
+    try:
+        response = client.tag_policy(
+            PolicyArn= policy,
+            Tags=[
+                {
+                    'Key': 'creatorId',
+                    'Value': principaID,
+                },
+                {
+                    'Key': 'UserName',
+                    'Value': userName,
+                },
+                {
+                    'Key': 'create_at',
+                    'Value': now
+                },
+            ],
+        )
+    except Exception as e:
+        print (e)
+
+                
+def item_generator(json_input):
+    if isinstance(json_input, dict):
+        for k, v in json_input.items():
+            if(type(v)== str):
+                if re.match('^[(a-z)]+-[(a-z0-9)]',v.lower()) and "Id" in k : 
+                    if (k not in ("requestId","reservationId","ownerId","attachmentId","associationId")):
+                        yield v
+            else:
+                yield from item_generator(v)
+    elif isinstance(json_input, list):
+        for item in json_input:
+            yield from item_generator(item)
+            
+
+def addtagResource(resourcegroupstaggingapi,userName,principaID, arn):
+    try:
+        resourcegroupstaggingapi.tag_resources(
+            ResourceARNList=[
+                arn,
+            ],
+            Tags={
+                'creatorId': principaID,"UserName":userName,"create_at":now
+            }
+        )
+    except Exception as e:
+        print (e)
+        
+def addtagLogGruop(client,userName,principaID, arn):
+    try:
+        client.tag_log_group(
+            logGroupName= arn
+            ,
+            tags={
+                'creatorId': principaID,"UserName":userName,"create_at":now
+            }
+        )
+    except Exception as e:
+        print (e)
+        
+        
+        
+    
